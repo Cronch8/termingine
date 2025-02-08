fe437c3c7166c8df4aac78b7ee673a108a27b45d
Mark Vilbiks <amazing.homan@gmail.com>
2025-02-08 18:59:28 +0200

    fixed buffer overflow

--- engine.c
+++ engine.c
@@ -8,79 +8,79 @@
 #include <sys/types.h>
 
 typedef struct {
-    uint x;
-    uint y;
-    uint brightness;
+  uint x;
+  uint y;
+  uint brightness;
 } TE_particle;
 
 typedef struct {
-    int width;
-    int height;
-    int particle_count;
-    TE_particle* particles;
+  int width;
+  int height;
+  int particle_count;
+  TE_particle *particles;
 } TE_particle_layer;
 
 // i'm basically trusting that the compiler will know to optimize out this
 // loop here, else this will be slow.
-// Returns the ascii character representing it's brightness. By default has 13 characters.
+// Returns the ascii character representing it's brightness. By default has 13
+// characters.
 char TE_get_brightness_char(int n) {
-    char * chars =  " .-:=csZ58#M@";
-    int length = 0;
-    for (int i = 0; chars[i] != '\0'; i++) {
-        length = i;
-    }
-    if (n > length) return chars[length];
-    if (n < 0) return chars[0];
-    return chars[n];
+  char *chars = " .-:=csZ58#M@";
+  int length = 0;
+  for (int i = 0; chars[i] != '\0'; i++) {
+    length = i;
+  }
+  if (n > length)
+    return chars[length];
+  if (n < 0)
+    return chars[0];
+  return chars[n];
 }
 
-// Makes a particle layer objet, the core object around which all particle operations work.
-// Returns NULL enough memory can't be allocated.
-TE_particle_layer* TE_create_particle_layer(uint width, uint height, int particle_count) {
-    TE_particle_layer* layer = malloc(sizeof(TE_particle_layer));
-    if (!layer) return NULL;
-    layer->width = width;
-    layer->height = height;
-    layer->particle_count = particle_count;
+// Makes a particle layer objet, the core object around which all particle
+// operations work. Returns NULL enough memory can't be allocated.
+TE_particle_layer *TE_create_particle_layer(uint width, uint height,
+                                            int particle_count) {
+  TE_particle_layer *layer = malloc(sizeof(TE_particle_layer));
+  if (!layer)
+    return NULL;
+  layer->width = width;
+  layer->height = height;
+  layer->particle_count = particle_count;
 
-    layer->particles = calloc(particle_count, sizeof(TE_particle));
-    if (!layer->particles) {
-        free(layer);
-        return NULL;
-    }
-    return layer;
-}
-
-void TE_destory_particle_layer(TE_particle_layer* layer) {
-    free(layer->particles);// is this correct? is it freeing the whole array?
+  layer->particles = calloc(particle_count, sizeof(TE_particle));
+  if (!layer->particles) {
     free(layer);
+    return NULL;
+  }
+  return layer;
+}
+
+void TE_destory_particle_layer(TE_particle_layer *layer) {
+  free(layer->particles); // is this correct? is it freeing the whole array?
+  free(layer);
 }
 
 // update is the fn provided by the user that will update each particle once.
 // Can be used to initialize the particles in specific postions too.
-void TE_update_particles(TE_particle_layer* layer, void (*update)(TE_particle_layer*, TE_particle*)) {
-    printf("nnnn");
-    fflush(stdout);
-    printf("%d", layer->particle_count);
-    printf("wwww");
-    fflush(stdout);
-    for (int i = 0; i < layer->particle_count; i++) {
-        printf(">%d", i);
-        fflush(stdout);
-        update(layer, &layer->particles[i]);
-    }
+void TE_update_particles(TE_particle_layer *layer,
+                         void (*update)(TE_particle_layer *, TE_particle *)) {
+  for (int i = 0; i < layer->particle_count; i++) {
+    update(layer, &layer->particles[i]);
+  }
 }
 
 // return the array of chars for the whole window.
 // Chars needs to be at least width*height*sizeof(char) in size
-void TE_calculate_chars(TE_particle_layer* layer, char* chars) {
-    memset(chars, 0, layer->height * layer->width * sizeof(char));
-    for (int i = 0; i < layer->particle_count;  i++) {
-        chars[layer->particles[i].y * layer->width + layer->particles[i].x] += layer->particles[i].brightness;
-    }
-    for (int i = 0; i < layer->width*layer->height;  i++) {
-        chars[i] = TE_get_brightness_char(chars[i]);
-    }
+void TE_calculate_chars(TE_particle_layer *layer, char *chars) {
+  memset(chars, 0, layer->height * layer->width * sizeof(char));
+  for (int i = 0; i < layer->particle_count; i++) {
+    chars[layer->particles[i].y * layer->width + layer->particles[i].x] +=
+        layer->particles[i].brightness;
+  }
+  for (int i = 0; i < layer->width * layer->height; i++) {
+    chars[i] = TE_get_brightness_char(chars[i]);
+  }
 }
 
 #endif
--- main.c
+++ main.c
@@ -9,52 +9,49 @@ uint width = 240;
 uint height = 55;
 
 void set_random_particle(TE_particle_layer *layer, TE_particle *particle) {
-    particle->x = random() % width;
-    particle->y = random() % height;
-    particle->brightness = random() % 13;// 13 is the number of chars in the " .-:=csZ58#M@" string
+  particle->x = random() % width;
+  particle->y = random() % height;
+  particle->brightness =
+      random() % 13; // 13 is the number of chars in the " .-:=csZ58#M@" string
 }
 
 void decay_particle(TE_particle_layer *layer, TE_particle *particle) {
-    if (particle->brightness <= 0) {
-        particle->x = random() % width;
-        particle->y = 0;
-        particle->brightness = random() % 18 + 5;
-    } else particle->brightness -= 1;
-    if (particle->y >= layer->height) particle->y = 0;
-    else particle->y += 1;
+  if (particle->brightness <= 0) {
+    particle->x = random() % width;
+    particle->y = 0;
+    particle->brightness = random() % 18 + 5;
+  } else
+    particle->brightness -= 1;
+  if (particle->y >= layer->height)
+    particle->y = 0;
+  else
+    particle->y += 1;
 }
 
 int main() {
-    TE_particle_layer *layer = TE_create_particle_layer(width, height, 100);
-    TE_update_particles(layer, set_random_particle);
-    int size = width * height;
-    char window_chars[size+1];
-    memset(&window_chars, 0, size);
-    window_chars[size+1] = '\0';
-    struct timespec ts;
-    double dt = 0.07;
-    ts.tv_sec = dt;
-    ts.tv_nsec = (int)(dt*1000000000) % 1000000000;
-    while (1) {
-        printf("0000");
-        fflush(stdout);
-        //printf("raw data:%n", (int*)(void*)layer);
-        fflush(stdout);
-        printf("count:%d", layer->particle_count);
-        fflush(stdout);
-        TE_update_particles(layer, decay_particle);
-        printf("1111");
-        fflush(stdout);
-        TE_calculate_chars(layer, window_chars);
-        printf("2222");
-        fflush(stdout);
-        window_chars[size+1] = '\0';
-        printf("3333");
-        fflush(stdout);
-        printf("%s", window_chars);
-        fflush(stdout);
-        printf("4444");
-        fflush(stdout);
-        nanosleep(&ts, &ts);
-    }
+  time_t timer = 0;
+  time(&timer);
+  printf("testinng ");
+  fflush(stdout);
+  printf("time: %ld ", timer);
+  fflush(stdout);
+  srand(timer);
+  TE_particle_layer *layer = TE_create_particle_layer(width, height, 100);
+  TE_update_particles(layer, set_random_particle);
+  int size = width * height * sizeof(char);
+  char *window_chars = calloc(size + 1, sizeof(char));
+  window_chars[size] = '\0';
+  struct timespec ts;
+  double dt = 0.033;
+  ts.tv_sec = dt;
+  ts.tv_nsec = (int)(dt * 1000000000) % 1000000000;
+  while (1) {
+    TE_update_particles(layer, decay_particle);
+    TE_calculate_chars(layer, window_chars);
+    window_chars[size] = '\0';
+    printf("%s", window_chars);
+    fflush(stdout);
+    nanosleep(&ts, &ts);
+  }
+  free(window_chars);
 }
